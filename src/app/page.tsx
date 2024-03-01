f91d53cf74dc31d11eaa0d9def832e42f9cfa800
Krrish Halder <krrish.code07@gmail.com>
2024-03-01 22:20:05 +0530

    Add UpcomingWebinar component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinar from "@/components/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
         <FeaturedCourses />
         <WhyChooseUs />
         <TestimonialCards />
+        <UpcomingWebinar />
       </main>
     </div>
   );
